492d31df94a6d2676f6235136f96eb18537e94dd
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2022-02-18 11:06:59 +0100

    feat(playwright): rename example executor to test

--- packages/playwright/src/executors/test/executor.spec.ts
+++ packages/playwright/src/executors/test/executor.spec.ts
@@ -1,9 +1,9 @@
-import { BuildExecutorSchema } from './schema';
+import { TestExecutorSchema } from './schema';
 import executor from './executor';
 
-const options: BuildExecutorSchema = {};
+const options: TestExecutorSchema = {};
 
-describe('Build Executor', () => {
+describe('Test Executor', () => {
   it('can run', async () => {
     const output = await executor(options);
     expect(output.success).toBe(true);
