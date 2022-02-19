500eaaceb7c861465f7fd9cfe8f2d5d5876d59ae
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2022-02-19 11:30:29 +0100

    feat(playwright): add test executor schema and interface

--- packages/playwright/src/executors/test/schema.d.ts
+++ packages/playwright/src/executors/test/schema.d.ts
@@ -1,1 +1,17 @@
-export interface TestExecutorSchema {} // eslint-disable-line
+export interface TestExecutorSchema {
+  playwrightConfig: string;
+  outputDir: string;
+  devServerTarget?: string;
+  headed?: boolean;
+  record?: boolean;
+  parallel?: boolean;
+  browser?: string;
+  debug?: boolean;
+  env?: Record<string, string>;
+  spec?: string;
+  ignoreTestFiles?: string;
+  reporter?: string;
+  reporterOptions?: string;
+  skipServe?: boolean;
+  tag?: string;
+}
